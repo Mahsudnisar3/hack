11a22f4139fe316ee6893a8acd7256d2059c7ad7
Mahsudnisar3 <63830023+Mahsudnisar3@users.noreply.github.com>
2022-04-27 11:54:48 +0500

    Update nisarmahsud.py

--- nisarmahsud.py
+++ nisarmahsud.py
@@ -11,6 +11,16 @@ else:
 
 
 
+import platform
+
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+          __import__("Dump").nisarmsd()
+elif 'aarch' in arc:
+              __import__("Dump").nisarmsd()
+else:
+        exit(f' Unknow device machine {arc}')
+
 
 	
 
