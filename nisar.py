eebbe878de1fdfbd59e663841e785f14335727fd
Mahsudnisar3 <63830023+Mahsudnisar3@users.noreply.github.com>
2022-04-27 17:02:29 +0500

    Update nisar.py

--- nisar.py
+++ nisar.py
@@ -16,14 +16,8 @@ elif bit == '32bit':
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     nisarmsd()
-import os, platform,time
-try:
-   import requests
-except:
-   os.system('pip2 install requests')
-from time import sleep
-import requests
-bit = platform.architecture()[0]
+
+
 if bit == '64bit':
     from Dump import nisarmsd
     print("\n Welcome to Nisar Cloning Tool! \n Opps! Your ip Address Block \n")
